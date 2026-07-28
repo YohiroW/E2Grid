ee91d4b74ddcc1ee780aef6053ab7f55cf59fdef
YohiroW <yohiro36@foxmail.com>
2026-07-28 15:38:39 +0800

    Add game module

---
+++ Source/E2GridGame/E2GridGame.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class E2GridGame : ModuleRules
+{
+    public E2GridGame(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/E2GridGame/Private/E2GridGame.cpp
@@ -0,0 +1,17 @@
+﻿#include "E2GridGame.h"
+
+#define LOCTEXT_NAMESPACE "FE2GridGameModule"
+
+void FE2GridGameModule::StartupModule()
+{
+    
+}
+
+void FE2GridGameModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FE2GridGameModule, E2GridGame)
